060139b26f35caf0371bb25776ca88b3237608f9
georginafp <66628267+georginafp@users.noreply.github.com>
2022-04-07 09:10:57 +0200

    Update 003_PRS_BW.sh

--- 003_PRS_BW.sh
+++ 003_PRS_BW.sh
@@ -1,3 +1,4 @@
+
 Rscript /home/isglobal.lan/gfuentes/data/WS_HELIX/HELIX_preproc/pgrs/scripts/Templates_PRSice_v2.0/PRSice.R --dir . \
 --prsice /home/isglobal.lan/gfuentes/data/WS_HELIX/HELIX_preproc/pgrs/scripts/Templates_PRSice_v2.0/PRSice_linux \
 --base /home/isglobal.lan/gfuentes/data/WS_HELIX/HELIX_preproc/pgrs/base_data/002_BW/base_data.txt \
@@ -22,3 +23,4 @@ Rscript /home/isglobal.lan/gfuentes/data/WS_HELIX/HELIX_preproc/pgrs/scripts/Tem
 --perm 10000 \
 --all-score \
 --out /home/isglobal.lan/gfuentes/data/WS_HELIX/HELIX_preproc/pgrs/results/002_BW/BW_EUR/HELIX_zBW
+
